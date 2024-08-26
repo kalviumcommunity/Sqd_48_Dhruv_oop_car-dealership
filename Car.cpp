66270133d78ec3b461a86166dd160158819aad85
dhruvk1804 <dhruvk1804@gmail.com>
2024-08-26 12:40:17 +0530

    this pointer

--- Car.cpp
+++ Car.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 class Car {
 private:
     string make;
     string model;
     int year;
+    double price;
 
 public:
     // Constructor
-    Car(string mk, string mdl, int yr)
-        : make(mk), model(mdl), year(yr) {}
+    Car(string mk, string mdl, int yr, double prc)
+        : make(mk), model(mdl), year(yr), price(prc) {}
 
     // Member function 1: Display car details
     void displayDetails() {
-        cout << "Car: " << make << " " << model << ", Year: " << year << endl;
+        cout << "Car: " << make << " " << model << ", Year: " << year << ", Price: $" << price << endl;
     }
 
     // Member function 2: Update year
-    void updateYear(int newYear) {
-        year = newYear;
+    Car* updateYear(int newYear) {
+        this->year = newYear;
+        return this; // Enable method chaining
+    }
+
+    // Member function 3: Update price
+    Car* updatePrice(double newPrice) {
+        this->price = newPrice;
+        return this; // Enable method chaining
     }
 };
+
