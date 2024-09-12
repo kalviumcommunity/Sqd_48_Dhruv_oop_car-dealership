1d42ad442f66d277e9a224b274346051045e0c7e
dhruvk1804 <dhruvk1804@gmail.com>
2024-09-12 11:49:11 +0530

    new and delete

--- Car.cpp
+++ Car.cpp
@@ -1,5 +1,38 @@
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// class Car {
+// private:
+//     string make;
+//     string model;
+//     int year;
+//     double price;
+
+// public:
+//     // this is Constructor
+//     Car(string mk, string mdl, int yr, double prc)
+//         : make(mk), model(mdl), year(yr), price(prc) {}
+
+//     // Member function for Displaying car details
+//     void displayDetails() {
+//         cout << "Car: " << make << " " << model << ", Year: " << year << ", Price: $" << price << endl;
+//     }
+
+//     // Member function for Updating year
+//     Car* updateYear(int newYear) {
+//         this->year = newYear;
+//         return this; 
+//     }
+
+//     // Member function for Updating price
+//     Car* updatePrice(double newPrice) {
+//         this->price = newPrice;
+//         return this; 
+//     }
+// };
+
 #include <iostream>
-#include <string>
 using namespace std;
 
 class Car {
@@ -10,25 +43,22 @@ private:
     double price;
 
 public:
-    // this is Constructor
-    Car(string mk, string mdl, int yr, double prc)
-        : make(mk), model(mdl), year(yr), price(prc) {}
+    // Constructor to initialize Car object
+    Car(string make = "", string model = "", int year = 0, double price = 0.0) 
+        : make(make), model(model), year(year), price(price) {}
 
-    // Member function for Displaying car details
-    void displayDetails() {
+    // Member function to display Car details
+    void displayDetails() const {
         cout << "Car: " << make << " " << model << ", Year: " << year << ", Price: $" << price << endl;
     }
 
-    // Member function for Updating year
-    Car* updateYear(int newYear) {
-        this->year = newYear;
-        return this; 
+    // Member function to update the year of the car
+    void updateYear(int newYear) {
+        year = newYear;
     }
 
-    // Member function for Updating price
-    Car* updatePrice(double newPrice) {
-        this->price = newPrice;
-        return this; 
+    // Destructor to confirm object destruction
+    ~Car() {
+        cout << "Car " << make << " " << model << " destroyed." << endl;
     }
 };
-
--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,55 @@
+// #include "Car.cpp"
+// #include <iostream>
+
+// int main() {
+//     Car cars[3] = {
+//         Car("Toyota", "Camry", 2020, 30000.00),
+//         Car("Honda", "Civic", 2019, 25000.00),
+//         Car("Ford", "Mustang", 2021, 40000.00)
+//     };
+
+//     for (int i = 0; i < 3; i++) {
+//         cars[i].displayDetails();
+//     }
+
+//     cars[0].updateYear(2023)->updatePrice(32000.00);
+
+//     cars[0].displayDetails();
+
+//     return 0;
+// }
+
 #include "Car.cpp"
 #include <iostream>
+using namespace std;
 
 int main() {
-    Car cars[3] = {
+    int numCars = 3; // Fixed number of cars for demonstration
+
+    // Dynamically allocate memory for an array of Car objects
+    Car* cars = new Car[numCars] {
         Car("Toyota", "Camry", 2020, 30000.00),
         Car("Honda", "Civic", 2019, 25000.00),
         Car("Ford", "Mustang", 2021, 40000.00)
     };
 
-    for (int i = 0; i < 3; i++) {
+    // Display details of all cars
+    for (int i = 0; i < numCars; i++) {
         cars[i].displayDetails();
     }
 
-    cars[0].updateYear(2023)->updatePrice(32000.00);
+    // Update the year of the first car
+    cars[0].updateYear(2023);
+    // Update the price of the first car
+    cars[0].price = 32000.00;
 
-    cars[0].displayDetails();
+    // Display updated details of all cars
+    for (int i = 0; i < numCars; i++) {
+        cars[i].displayDetails();
+    }
+
+    // Release dynamically allocated memory for Car objects
+    delete[] cars;
 
     return 0;
 }
