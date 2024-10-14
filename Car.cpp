363af145c301fc038713718f5b775ec9dd314d8c
dhruvk1804 <dhruvk1804@gmail.com>
2024-10-14 13:16:30 +0530

    Encapsulation

--- Car.cpp
+++ Car.cpp
@@ -1,3 +1,36 @@
+// #include <iostream>
+// using namespace std;
+
+// class Car {
+//     string make;
+//     string model;
+//     int year;
+//     double price;
+
+// public:
+//     static int carCount; // Static variable to count the number of Car objects
+
+//     Car(string make, string model, int year, double price) 
+//         : make(make), model(model), year(year), price(price) {
+//         carCount++; // Increment count on creation
+//     }
+
+//     void displayDetails() const {
+//         cout << "Car: " << make << " " << model << ", Year: " << year << ", Price: $" << price << endl;
+//     }
+
+//     static int getCarCount() { // Static member function to get the count of cars
+//         return carCount;
+//     }
+
+//     ~Car() {
+//         carCount--; // Decrement count on destruction
+//     }
+// };
+
+// // Initialize static variable
+// int Car::carCount = 0;
+
 #include <iostream>
 using namespace std;
 
@@ -8,25 +41,24 @@ class Car {
     double price;
 
 public:
-    static int carCount; // Static variable to count the number of Car objects
+    // Constructor to initialize Car object
+    Car(string make, string model, int year, double price)
+        : make(make), model(model), year(year), price(price) {}
 
-    Car(string make, string model, int year, double price) 
-        : make(make), model(model), year(year), price(price) {
-        carCount++; // Increment count on creation
+    // Accessor (getter) for price
+    double getPrice() const {
+        return price;
     }
 
+    // Mutator (setter) for price
+    void setPrice(double newPrice) {
+        if (newPrice > 0) {
+            price = newPrice;
+        }
+    }
+
+    // Member function to display Car details
     void displayDetails() const {
         cout << "Car: " << make << " " << model << ", Year: " << year << ", Price: $" << price << endl;
     }
-
-    static int getCarCount() { // Static member function to get the count of cars
-        return carCount;
-    }
-
-    ~Car() {
-        carCount--; // Decrement count on destruction
-    }
 };
-
-// Initialize static variable
-int Car::carCount = 0;
