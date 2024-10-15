580ef8fb9e054db3ab39dd5727e0bb1ab99daa77
dhruvk1804 <dhruvk1804@gmail.com>
2024-10-15 12:01:17 +0530

    Constructors and Destructors

--- Car.cpp
+++ Car.cpp
@@ -1,18 +1,70 @@
+// #include <iostream>
+// using namespace std;
+
+// class Car {
+// private:
+//     // Private data members to hide internal details
+//     string make;
+//     string model;
+//     int year;
+//     double price;
+
+// public:
+//     // Constructor to initialize Car object
+//     Car(string make, string model, int year, double price)
+//         : make(make), model(model), year(year), price(price) {}
+
+//     // Accessor (getter) for price
+//     double getPrice() const {
+//         return price;
+//     }
+
+//     // Mutator (setter) for price
+//     void setPrice(double newPrice) {
+//         if (newPrice > 0) {
+//             price = newPrice;
+//         }
+//     }
+
+//     // Accessor (getter) for year
+//     int getYear() const {
+//         return year;
+//     }
+
+//     // Mutator (setter) for year
+//     void setYear(int newYear) {
+//         if (newYear > 1885) { // Cars were not made before 1885
+//             year = newYear;
+//         }
+//     }
+
+//     // Member function to display Car details
+//     void displayDetails() const {
+//         cout << "Car: " << make << " " << model << ", Year: " << year << ", Price: $" << price << endl;
+//     }
+// };
+
 #include <iostream>
 using namespace std;
 
 class Car {
-private:
-    // Private data members to hide internal details
+   private: 
     string make;
     string model;
     int year;
     double price;
 
 public:
-    // Constructor to initialize Car object
+    // Default Constructor
+    Car() : make("Unknown"), model("Unknown"), year(0), price(0.0) {
+        cout << "Default Car constructor called." << endl;
+    }
+
+    // Parameterized Constructor
     Car(string make, string model, int year, double price)
-        : make(make), model(model), year(year), price(price) {}
+        : make(make), model(model), year(year), price(price) {
+        cout << "Parameterized Car constructor called." << endl;
+    }
 
     // Accessor (getter) for price
     double getPrice() const {
@@ -26,20 +78,13 @@ public:
         }
     }
 
-    // Accessor (getter) for year
-    int getYear() const {
-        return year;
-    }
-
-    // Mutator (setter) for year
-    void setYear(int newYear) {
-        if (newYear > 1885) { // Cars were not made before 1885
-            year = newYear;
-        }
-    }
-
     // Member function to display Car details
     void displayDetails() const {
         cout << "Car: " << make << " " << model << ", Year: " << year << ", Price: $" << price << endl;
+    }
+
+    // Destructor
+    ~Car() {
+        cout << "Destructor called for Car: " << make << " " << model << endl;
     }
 };
--- Owner.cpp
+++ Owner.cpp
@@ -1,15 +1,64 @@
+// #include <iostream>
+// using namespace std;
+
+// class Owner {
+// private:
+//     // Private data members to hide internal details
+//     string name;
+//     int age;
+
+// public:
+//     // Constructor to initialize Owner object
+//     Owner(string name, int age) : name(name), age(age) {}
+
+//     // Accessor (getter) for name
+//     string getName() const {
+//         return name;
+//     }
+
+//     // Mutator (setter) for name
+//     void setName(string newName) {
+//         if (!newName.empty()) {
+//             name = newName;
+//         }
+//     }
+
+//     // Accessor (getter) for age
+//     int getAge() const {
+//         return age;
+//     }
+
+//     // Mutator (setter) for age
+//     void setAge(int newAge) {
+//         if (newAge > 0) {
+//             age = newAge;
+//         }
+//     }
+
+//     // Member function to display Owner details
+//     void displayDetails() const {
+//         cout << "Owner: " << name << ", Age: " << age << endl;
+//     }
+// };
+
 #include <iostream>
 using namespace std;
 
 class Owner {
-private:
-    // Private data members to hide internal details
-    string name;
-    int age;
+    private:
+       string name;
+       int age;
 
 public:
-    // Constructor to initialize Owner object
-    Owner(string name, int age) : name(name), age(age) {}
+    // Default Constructor
+    Owner() : name("Unknown"), age(0) {
+        cout << "Default Owner constructor called." << endl;
+    }
+
+    // Parameterized Constructor
+    Owner(string name, int age) : name(name), age(age) {
+        cout << "Parameterized Owner constructor called." << endl;
+    }
 
     // Accessor (getter) for name
     string getName() const {
@@ -23,20 +72,13 @@ public:
         }
     }
 
-    // Accessor (getter) for age
-    int getAge() const {
-        return age;
-    }
-
-    // Mutator (setter) for age
-    void setAge(int newAge) {
-        if (newAge > 0) {
-            age = newAge;
-        }
-    }
-
     // Member function to display Owner details
     void displayDetails() const {
         cout << "Owner: " << name << ", Age: " << age << endl;
+    }
+
+    // Destructor
+    ~Owner() {
+        cout << "Destructor called for Owner: " << name << endl;
     }
 };
